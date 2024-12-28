04a606b7cfe38ae0dee8d64beb54a749c005490a
arimu <arimurahiroaki40@gmail.com>
2024-12-28 17:36:37 +0900

    cssmodulesを追加

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,5 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
--- components/Headline.jsx
+++ components/Headline.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "./Headline.module.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
