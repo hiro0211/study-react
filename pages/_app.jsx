d084b5007c92c61905165b57ab0f327fa6a273de
arimu <arimurahiroaki40@gmail.com>
2025-01-05 18:31:21 +0900

    useRouterを学習

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,12 @@
 import "../src/styles/globals.css";
 import { useCounter } from "../src/hooks/useCounter";
 import { useArray } from "@/src/hooks/useArray";
-import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
+import { useBgColor } from "@/src/hooks/useBgColor";
 
 export default function App({ Component, pageProps }) {
   const counter = useCounter();
   const inputArray = useArray();
-  useBgLightBlue();
+  useBgColor();
 
   return (
     <>
